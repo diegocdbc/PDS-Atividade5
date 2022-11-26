8432f96a9c18e166d2161325aba1de84d556da40
diegocdbc <diegocdbc2@gmail.com>
2022-11-26 09:26:34 -0300

    chore: Innitial commit

---
+++ ATCMediator/ATCMediator.java
@@ -0,0 +1,15 @@
+package ATCMediator;
+
+import CommandClass.Flight;
+import CommandClass.Runway;
+
+public interface ATCMediator {
+    public void registerRunway(Runway runway);
+
+    public void registerFlight(Flight flight);
+
+    public boolean isLandingOk();
+
+    public void setLandingStatus(boolean status);
+
+}
---
+++ CommandClass/Command.java
@@ -0,0 +1,12 @@
+package CommandClass;
+
+/**
+ * Interface que define os métodos que cada avião deverá implementar.
+ * Os métodos dizem respeito às ações de pouso de cada avião (vôo)
+ * 
+ * @author alex
+ *
+ */
+public interface Command {
+    void land();
+}
